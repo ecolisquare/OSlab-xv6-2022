8f7b0d4e8bdc638357f26d67928abdb6aac3780b
ecolisquare <1427359791@qq.com>
2022-10-25 02:39:38 -0700

    Task print a page table has been finished

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprintlevel(pagetable_t pagetable, int level) {
+    char *delim;
+    if (level == 2) delim = "..";
+    if (level == 1) delim = ".. ..";
+    if (level == 0) delim = ".. .. ..";
+    for (int i = 0; i < 512; i++) {
+        pte_t pte = pagetable[i];
+        if ((pte & PTE_V)) {
+            //  this PTE points to a lower level page table.
+            printf("%s%d: pte %p pa %p\n", delim, i, pte, PTE2PA(pte));
+            uint64 child = PTE2PA(pte);
+            if (level != 0) {
+                vmprintlevel((pagetable_t)child, level - 1);
+            }
+        }
+    }
+}
+
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  vmprintlevel(pagetable, 2);
+}
